27833d4310324a9f853f6a89e005068c988a6cb3
Karsten Westra <karstenwestra@gmail.com>
2021-02-15 23:14:13 +0100

    Created very simpel terrain with click to move and follow camera.

---
+++ RPG-Unity/Assets/Gifgroen/Scripts/Locomotion/FollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Gifgroen.Locomotion
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField] private Camera mainCamera;
+
+        [SerializeField] private Transform followTarget;
+
+        private void Update()
+        {
+            transform.position = followTarget.position;
+        }
+    }
+}
---
+++ RPG-Unity/Assets/Gifgroen/Scripts/Locomotion/Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Gifgroen.Locomotion
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent navMeshAgent;
+
+        [SerializeField] private Camera mainCamera;
+
+        private void OnValidate()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            if (!Input.GetMouseButtonUp(0)) return;
+            
+            Ray r = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(r, out RaycastHit hit))
+            {
+                navMeshAgent.destination = hit.point;
+            }
+        }
+    }
+}
